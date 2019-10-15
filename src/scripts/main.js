96912ae4e4b6d71c6a0719f6d9947caead97d3d5
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-15 14:48:59 -0500

    Corrected code and rewrote it another way

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+// let conjunction = function (firstWord, secondWord) {
+//     let conjoinedWord = `${firstWord} ${secondWord}`
+//     console.log(conjoinedWord)
+// }
+
+// conjunction("Master", "Card")
+
+
+// Thought the original code I edited wasn't working so I re-wrote it as a big arrow function
+// Link your damn JS in the html, Cassie! 
+
+const conjunction = (firstWord, secondWord) => {
+const conjoinedWord = `${firstWord} ${secondWord}`
+console.log(conjoinedWord)
+}
+
+conjunction("Master", "Card")
